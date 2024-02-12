fdbd9f3583099304bb30fe94af2f4c499c50a07d
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-12 20:58:01 +0530

    Importing function from components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ExampleState from "./Components/Statehook/State"
 import Example2 from "./Components/Statehook/State2"
+import ExampleEffect from "./Components/Effects/Effects"
 function App (){
   return (
     <>
       <h2 className='text-3xl  bg-slate-400 border text-center text-white'>Hello Shri Vishnu ,Let's Learn React Hooks</h2>
       {/* <ExampleState/> */}
-      <Example2 />
+      {/* <Example2 /> */}
+      <ExampleEffect />
     
     </>
   )
