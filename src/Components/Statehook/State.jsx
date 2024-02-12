b47de93f25bd579fe83726dff445138c338286ea
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-12 13:07:18 +0530

    useState simple example using counter

--- src/Components/Statehook/State.jsx
+++ src/Components/Statehook/State.jsx
@@ -1,10 +1,32 @@
 import { useState } from "react";
 
-function ExamppleState(){
-    return(
-        <>
-        </>
-    )
+function ExampleState() {
+  const [count, Setcount] = useState(0);
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <h1>Your count is {count}</h1>
+        <div>
+          <button
+            onClick={() => {
+              Setcount(count + 1);
+            }}
+            className="border bg-green-500 rounded-md p-3 text-white"
+          >
+            Increment
+          </button>
+          <button
+            onClick={() => {
+              Setcount(count -1);
+            }}
+            className="border bg-green-500 rounded-md p-3 text-white"
+          >
+            decrement
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ExamppleState;
+export default ExampleState;
