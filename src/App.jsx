323fcf06b00c2d2a220d493619a141ebc8acb78e
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-12 20:51:05 +0530

    edited classNaame

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
+import ExampleState from "./Components/Statehook/State"
+import Example2 from "./Components/Statehook/State2"
 function App (){
   return (
     <>
-      <h2 className='text-3xl text-blue-800'>Heloo here vishnu</h2>
+      <h2 className='text-3xl  bg-slate-400 border text-center text-white'>Hello Shri Vishnu ,Let's Learn React Hooks</h2>
+      {/* <ExampleState/> */}
+      <Example2 />
+    
     </>
   )
 }
